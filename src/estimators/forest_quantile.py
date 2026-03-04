817ff1572716756521da8b78f87901d6224331f3
Nick Kunz <nhk37@cornell.edu>
2026-03-04 12:46:49 -0800

    Refactor ForestQuantile and ForestBase constructors: Add type hints and improve readability

--- src/estimators/forest_quantile.py
+++ src/estimators/forest_quantile.py
@@ -1,4 +1,7 @@
 ## libraries
+import numpy as np
+from typing import Any
+from numpy.typing import ArrayLike, NDArray
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn_quantile import RandomForestQuantileRegressor
 
@@ -7,18 +10,23 @@ from src.estimators.config import ASYMMETRY_C, ASYMMETRY_R
 
 ## random forest sklearn regressors
 class ForestQuantile:
-    def __init__(self, quantile_c = ASYMMETRY_C, quantile_r = ASYMMETRY_R, **kwargs):
+    def __init__(
+        self,
+        quantile_c: float = ASYMMETRY_C,
+        quantile_r: float = ASYMMETRY_R,
+        **kwargs: Any
+        ) -> None:
         self.estimator_c = ForestBase(quantile = quantile_c, **kwargs)
         self.estimator_r = ForestBase(quantile = quantile_r, **kwargs)
 
 ## random forest sklearn framework
 class ForestBase(BaseEstimator, RegressorMixin):
-    def __init__(self, quantile, **kwargs):
+    def __init__(self, quantile: float, **kwargs: Any) -> None:
         self.quantile = quantile
-        self.kwargs = kwargs
+        self.kwargs: dict[str, Any] = kwargs
 
     ## sklearn fit interface
-    def fit(self, X, y):
+    def fit(self, X: ArrayLike, y: ArrayLike) -> "ForestBase":
         self.model_ = RandomForestQuantileRegressor(
             q = [self.quantile],  ## quantile must be passed at construction time
             **self.kwargs
@@ -27,7 +35,7 @@ class ForestBase(BaseEstimator, RegressorMixin):
         return self
 
     ## sklearn predict interface
-    def predict(self, X):
+    def predict(self, X: ArrayLike) -> NDArray[np.float64]:
         preds = self.model_.predict(X)
         if preds.ndim == 1:
             return preds
