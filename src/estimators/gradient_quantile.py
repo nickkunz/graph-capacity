ce4329d9100480d0c5b998a849fd92c8586619e4
Nick Kunz <nhk37@cornell.edu>
2026-03-04 12:46:55 -0800

    Refactor BoostingQuantile constructor: Add type hints and improve readability

--- src/estimators/gradient_quantile.py
+++ src/estimators/gradient_quantile.py
@@ -1,4 +1,5 @@
 ## libraries
+from typing import Any
 from sklearn.base import BaseEstimator
 from sklearn.ensemble import GradientBoostingRegressor
 
@@ -7,17 +8,23 @@ from src.estimators.config import ASYMMETRY_C, ASYMMETRY_R
 
 ## gradient boosting sklearn regressors
 class BoostingQuantile(BaseEstimator):
-    def __init__(self, quantile_c = ASYMMETRY_C, quantile_r = ASYMMETRY_R, **kwargs):
+    def __init__(
+        self,
+        quantile_c: float = ASYMMETRY_C,
+        quantile_r: float = ASYMMETRY_R,
+        **kwargs: Any
+        ) -> None:
+
         self.quantile_c = quantile_c
         self.quantile_r = quantile_r
-        self.kwargs = kwargs
+        self.kwargs: dict[str, Any] = kwargs
         self.estimator_c = GradientBoostingRegressor(
             loss = "quantile", 
-            alpha = quantile_c, 
+            alpha = quantile_c,  ## quantile, not regularization
             **kwargs
         )
         self.estimator_r = GradientBoostingRegressor(
             loss = "quantile", 
-            alpha = quantile_r, 
+            alpha = quantile_r,  ## quantile, not regularization
             **kwargs
         )
