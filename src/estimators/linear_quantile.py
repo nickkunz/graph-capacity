3bbddd98c625e9b72c2036c1c10d729a918f518d
Nick Kunz <nhk37@cornell.edu>
2026-03-04 12:47:13 -0800

    Refactor LinearQuantile constructor: Add type hints and improve parameter handling

--- src/estimators/linear_quantile.py
+++ src/estimators/linear_quantile.py
@@ -3,22 +3,35 @@ from sklearn.base import BaseEstimator
 from sklearn.linear_model import QuantileRegressor
 
 ## modules
-from src.estimators.config import ASYMMETRY_C, ASYMMETRY_R
+from src.estimators.config import (
+    ASYMMETRY_C,
+    ASYMMETRY_R,
+    ALPHA_C,
+    ALPHA_R
+)
 
 ## linear quantile sklearn regressors
 class LinearQuantile(BaseEstimator):
-    def __init__(self, quantile_c = ASYMMETRY_C, quantile_r = ASYMMETRY_R, alpha = 0.1, solver = "highs"):
+    def __init__(self, 
+        quantile_c: float = ASYMMETRY_C, 
+        quantile_r: float = ASYMMETRY_R, 
+        alpha_c: float = ALPHA_C, 
+        alpha_r: float = ALPHA_R,
+        solver: str = "highs"
+        ) -> None:
+
         self.quantile_c = quantile_c
         self.quantile_r = quantile_r
-        self.alpha = alpha
+        self.alpha_c = alpha_c
+        self.alpha_r = alpha_r
         self.solver = solver
         self.estimator_c = QuantileRegressor(
             quantile = quantile_c, 
-            alpha = alpha, 
+            alpha = alpha_c, 
             solver = solver
         )
         self.estimator_r = QuantileRegressor(
             quantile = quantile_r, 
-            alpha = alpha, 
+            alpha = alpha_r, 
             solver = solver
         )
