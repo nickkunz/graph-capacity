b0594320e734c138c764618e689dfabb01c0264f
Nick Kunz <nhk37@cornell.edu>
2026-03-18 00:04:37 -0700

    Remove unused imports and data loading functions from config.py

--- src/evaluators/config.py
+++ src/evaluators/config.py
@@ -1,20 +1,3 @@
-## libraries
-import pandas as pd
-from typing import Dict, Any
-
-## modules
-from src.estimators.linear_quantile import LinearQuantile
-from src.estimators.linear_laws import LinearLAWS
-from src.estimators.linear_convex import LinearConvex
-from src.estimators.forest_quantile import ForestQuantile
-from src.estimators.gradient_quantile import BoostingQuantile
-from src.estimators.xgboost_quantile import XGBoostQuantile
-from src.estimators.neural_networks import (
-    NeuralQuantile,
-    NeuralExpectile,
-    NeuralConvex
-)
-
 ## constants
 FEAT_X = [
     "n_nodes",
@@ -50,24 +33,3 @@ FEAT_Z = [
 ]
 TARGET = "target"
 
-## loaders
-def load_data(filepath: str = "../data/main.csv") -> pd.DataFrame:
-    return pd.read_csv(filepath)
-
-def load_models(input_dims: int = None) -> Dict[str, Any]:
-    return {
-        ## linear parametric
-        "linear_quantile": LinearQuantile(),
-        "linear_convex": LinearConvex(),
-        "linear_laws": LinearLAWS(),
-
-        ## non-parametric ensembles
-        "forest_quantile": ForestQuantile(),
-        "boosting_quantile": BoostingQuantile(),
-        "xgboost_quantile": XGBoostQuantile(),
-
-        ## neural networks
-        "neural_quantile": NeuralQuantile(input_dims = input_dims),
-        "neural_expectile": NeuralExpectile(input_dims = input_dims),
-        "neural_convex": NeuralConvex(input_dims = input_dims)
-    }
