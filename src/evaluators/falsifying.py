44df09e1c8168103dda07616bff8c60969262707
Nick Kunz <nhk37@cornell.edu>
2026-03-24 00:16:35 -0700

    Reorder import statements in falsifying.py for consistency

--- src/evaluators/falsifying.py
+++ src/evaluators/falsifying.py
@@ -2,9 +2,9 @@
 import sys
 import numpy as np
 import pandas as pd
-from typing import Dict, Any, Sequence
 from pathlib import Path
 from itertools import combinations
+from typing import Dict, Any, Sequence
 from joblib import Parallel, delayed
 from scipy.stats import wilcoxon
 
