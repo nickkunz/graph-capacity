54e5dbe262d346bd08987bd74318d16fed6db573
Nick Kunz <nhk37@cornell.edu>
2026-03-26 22:50:59 -0700

    Add random_state parameter to XGBoostQuantile for consistent initialization

--- src/estimators/xgboost_quantile.py
+++ src/estimators/xgboost_quantile.py
@@ -12,19 +12,23 @@ class XGBoostQuantile(BaseEstimator):
         self,
         quantile_c: float = ASYMMETRY_C,
         quantile_r: float = ASYMMETRY_R,
+        random_state: int = 42,
         **kwargs: Any
         ) -> None:
         
         self.quantile_c = quantile_c
         self.quantile_r = quantile_r
+        self.random_state = random_state
         self.kwargs: dict[str, Any] = kwargs
         self.estimator_c = xgb.XGBRegressor(
             objective = "reg:quantileerror",
             quantile_alpha = quantile_c,  ## quantile, not reguarization
+            random_state = random_state,
             **kwargs
         )
         self.estimator_r = xgb.XGBRegressor(
             objective = "reg:quantileerror",
             quantile_alpha = quantile_r,  ## quantile, not reguarization
+            random_state = random_state,
             **kwargs
         )
